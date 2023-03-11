58ea9c3311c9c1a089bef509cad4947e706f3276
serveryn44 <serveryn44@gmail.com>
2023-03-11 22:20:13 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var age = 33;
+char gender = 'F';
+
+
+
+if (gender == 'F' && age <= 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+
+}
+else if (name == "Ewa" && age == 33 && gender == 'F')
+{
+    Console.WriteLine("Ewa Lat 33");
+}
+
+ else if ( age < 18 && gender != 'F')
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else if (age > 33 && gender == 'F')
+{
+    Console.WriteLine("Nieznana Kobieta po 30-tce");
+}
+
+
+
+
+
+
+
