69bc758696450169d77d87bd3b15387f3c19fafa
serveryn44 <serveryn44@gmail.com>
2023-03-26 22:09:46 +0200

    zadanie domowe dzień 7

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeesTests.cs
@@ -0,0 +1,23 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void WhenEmployeSumUpScorsShooldReturnCorrectResul()
+        {
+            
+
+            var employee = new Employee("Adam", "Kowalski", 55);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(-5);
+            
+            
+            var result = employee.Result;
+
+            Assert.AreEqual(6, result);
+        }
+    }
+}
