45f87dd67155d7ef4885f767eb9001e695c20fe5
serveryn44 <serveryn44@gmail.com>
2023-03-31 21:43:51 +0200

    dzień 8 test typ referencyjny

--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -4,20 +4,44 @@ namespace ChallengeApp.Tests
 {
     public class TypeTests
     {
-        public void TEST()
+        public void GetEmployeeShouldReturnDifferentObjects()
         {
             // arrage
-            var employee1 = GetEmployee("Adam", "Kowalski", 22);
-            var employee2 = GetEmployee("Franek", "Nowak", 45);
+            var employee1 = GetEmployee("Adam");
+            var employee2 = GetEmployee("Adam");
             // act
 
 
             // assert
            Assert.AreNotEqual(employee1, employee2);
         }
-        private Employee GetEmployee(string name, string surname, int age)
+        private Employee GetEmployee(string name)
         {
-            return new Employee(name, surname, age);
+            return new Employee(name);
+        }
+
+        public void TestTwoNumbersAreEqual()
+        {
+            // arrage
+            int number1 = 1;
+            int number2 = 1;
+            // act
+
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        public void TestTwoStringAreNotEual()
+        {
+            // arrage
+            string number1 = "Adam";
+            string number2 = "Adam";
+            // act
+
+
+            // assert
+            Assert.AreNotEqual(number1, number2);
         }
 
 
